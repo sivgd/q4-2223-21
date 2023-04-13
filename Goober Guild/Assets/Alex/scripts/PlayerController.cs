5df08e90593bfc72b82ca5bef31de2a9a9e2c408
MSI\amlp0 <1545544@mukilteo.wednet.edu>
2023-04-13 15:11:33 -0700

    started on animations will try to finish tonight

--- Goober Guild/Assets/Alex/scripts/PlayerController.cs
+++ Goober Guild/Assets/Alex/scripts/PlayerController.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public bool isMoving;
+    
+    private bool isMoving;
     private Vector2 input;
 
     private void Update()
     {
         if (!isMoving)
         {
-            input.x = input.GetAxisRaw("Horizontal"):
-            input.y = input.GetAxisRaw("Vertical");
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0) input.y = 0;
 
             if (input != Vector2.zero)
             {
@@ -22,27 +25,22 @@ public class CharacterMovement : MonoBehaviour
                 targetPos.y += input.y;
 
                 StartCoroutine(Move(targetPos));
-            }
-
- 
+            } 
         }
-    }
+    
 
-    IEnumerator Move(Vector3 targetPos)
-    {
+        IEnumerator Move(Vector3 targetPos)
+        {
         isMoving = true;
 
-        while ((targerPos - transform.position).sqrMagnitude > mathf.Epsilon)
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transdorm.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = targetPos;
 
         isMoving = false;
+        }
     }
-
-
-
-
 }
