2903d44bfc8685af3c58c49609d32e304017cd6b
MarinaraSource <averagememe2022@gmail.com>
2023-04-17 11:05:09 -0700

    FIXED SOME STUFF

--- Goober Guild/Assets/Alex/scripts/PlayerController.cs
+++ Goober Guild/Assets/Alex/scripts/PlayerController.cs
@@ -38,27 +38,50 @@ public class PlayerController : MonoBehaviour
                     targetPos.x += input.x;
                     targetPos.y += input.y;
 
-                    StartCoroutine(Move(targetPos));
+                    if (IsWalkable(targetPos))
+                        StartCoroutine(Move(targetPos));
                 }
             }
 
             animator.SetBool("isMoving", isMoving);
 
         }
-    
+    }
 
-        IEnumerator Move(Vector3 targetPos)
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            isMoving = true;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
 
-            while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        isMoving = false;
+
+        CheckForEncounters();
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.1f, solidObjectsLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void CheckForEncounters()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
             {
-                transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-                yield return null;
+                Debug.Log("Ecountered a monster");
             }
-            transform.position = targetPos;
-
-            isMoving = false;
         }
     }
 }
