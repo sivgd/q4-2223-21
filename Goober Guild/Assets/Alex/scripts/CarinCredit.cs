7e228626f796cd83463716dbb402d76671ab2d68
MSI\amlp0 <1545544@mukilteo.wednet.edu>
2023-04-25 22:41:16 -0700

    Credits scene done

---
+++ Goober Guild/Assets/Alex/scripts/CarinCredit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarinCredit : MonoBehaviour
+{
+   public void finchCredit()
+   {
+		SceneManager.LoadScene("FinchCredit");
+   }
+}
+
