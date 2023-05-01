1cc3dc16a9e3e2d14ed0e58c41f7402c03712426
1545544@mukilteo.wednet.edu <1545544@mukilteo.wednet.edu>
2023-05-01 13:35:34 -0700

    things done

--- Goober Guild/Assets/Alex/scripts/Custscene.cs
+++ Goober Guild/Assets/Alex/scripts/Custscene.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitcher : MonoBehaviour
+public class Cutscene : MonoBehaviour
 {
-   public void cutscene()
-   {
-		SceneManager.LoadScene("Cutscene");
-   }
+	public void c()
+	{
+		SceneManager.LoadScene("CarinCredit");
+	}
 }
