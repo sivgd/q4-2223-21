70eab0502a716d66cc15aa410af0b641e49df4bf
MarinaraSource <averagememe2022@gmail.com>
2023-05-02 08:12:21 -0700

    Revert "UGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGh"
    This reverts commit 88d86c2fccc73e7f33efc53020c0a8ed079025f5.

--- Goober Guild/Assets/Logan/Scripts/Gameplay/Scene Details.cs
+++ Goober Guild/Assets/Logan/Scripts/Gameplay/Scene Details.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainerController : MonoBehaviour
+public class SceneDetails : MonoBehaviour
 {
+    [SerializeField] AudioClip sceneMusic;
+
     
 }
