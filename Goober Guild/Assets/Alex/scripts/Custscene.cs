0518c39ef2b222212da64f0a389ee01f8fc63462
MSI\amlp0 <1545544@mukilteo.wednet.edu>
2023-05-02 21:28:58 -0700

    made new script but cant attach becasue of errors

--- Goober Guild/Assets/Alex/scripts/Custscene.cs
+++ Goober Guild/Assets/Alex/scripts/Custscene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Custscene : MonoBehaviour
+public class Cutscene : MonoBehaviour
 {
 	public void TopDownMovement1()
 	{
